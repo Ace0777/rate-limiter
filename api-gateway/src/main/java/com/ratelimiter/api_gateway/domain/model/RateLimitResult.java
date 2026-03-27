0ed1ce03de6b4acfdd909a0e5be712869b123344
Ace0777 <luishhasantos@gmail.com>
2026-03-27 15:38:52 -0300

    feat: RateLimitResult class atritubutes and methods

--- api-gateway/src/main/java/com/ratelimiter/api_gateway/domain/model/RateLimitResult.java
+++ api-gateway/src/main/java/com/ratelimiter/api_gateway/domain/model/RateLimitResult.java
@@ -1,4 +1,34 @@
 package com.ratelimiter.api_gateway.domain.model;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class RateLimitResult {
+
+    private final boolean allowed;
+    private final int limit;
+    private final int remaining;
+    private final long resetEpochSeconds;
+    private final int retryAfterSeconds;   
+
+    public static RateLimitResult allowed(int limit, int remaining, long resetAt) {
+        return RateLimitResult.builder()
+                .allowed(true)
+                .limit(limit)
+                .remaining(remaining)
+                .resetEpochSeconds(resetAt)
+                .build();
+    }
+
+    public static RateLimitResult denied(int limit, long resetAt, int retryAfter) {
+        return RateLimitResult.builder()
+                .allowed(false)
+                .limit(limit)
+                .remaining(0)
+                .resetEpochSeconds(resetAt)
+                .retryAfterSeconds(retryAfter)
+                .build();
+    }
 }
