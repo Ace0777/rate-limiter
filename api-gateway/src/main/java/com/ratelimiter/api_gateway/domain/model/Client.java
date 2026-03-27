c33f1a6bbe57c337e38375c7ed44fa32365eabcc
Ace0777 <luishhasantos@gmail.com>
2026-03-27 15:32:09 -0300

    feat: creation of client class atributes and methods

--- api-gateway/src/main/java/com/ratelimiter/api_gateway/domain/model/Client.java
+++ api-gateway/src/main/java/com/ratelimiter/api_gateway/domain/model/Client.java
@@ -1,4 +1,25 @@
 package com.ratelimiter.api_gateway.domain.model;
 
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class Client {
+
+    private final String id;
+    private final String apiKey;
+    private String name;
+    private final Plan plan;
+    private final boolean active;
+
+    public boolean isAllowedToCall(){
+        return active;
+    }
+
+    //redis
+    public String rateLimitKey(String endpoint, String window) {
+        return String.format("rl:%s:%s:%s", apiKey, endpoint, window);
+    }
 }
--- api-gateway/src/main/java/com/ratelimiter/api_gateway/domain/service/RateLimiter.java
+++ api-gateway/src/main/java/com/ratelimiter/api_gateway/domain/service/RateLimiter.java
@@ -1,4 +1,8 @@
 package com.ratelimiter.api_gateway.domain.service;
 
+import com.ratelimiter.api_gateway.domain.model.Client;
+import com.ratelimiter.api_gateway.domain.model.RateLimitResult;
+
 public interface RateLimiter {
+    RateLimitResult check(Client client, String endpoint);
 }
