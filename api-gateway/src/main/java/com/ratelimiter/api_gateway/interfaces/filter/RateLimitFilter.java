e9bb2a647f60a96e8c2483f67703fb8f8378a6fb
Ace0777 <luishhasantos@gmail.com>
2026-03-31 14:11:29 -0300

    feat: add prometheus metrics, grafana dashboard and full docker compose setup

--- api-gateway/src/main/java/com/ratelimiter/api_gateway/interfaces/filter/RateLimitFilter.java
+++ api-gateway/src/main/java/com/ratelimiter/api_gateway/interfaces/filter/RateLimitFilter.java
@@ -2,6 +2,7 @@ package com.ratelimiter.api_gateway.interfaces.filter;
 
 import com.ratelimiter.api_gateway.application.usecase.CheckRateLimitUseCase;
 import com.ratelimiter.api_gateway.domain.model.RateLimitResult;
+import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import lombok.RequiredArgsConstructor;
@@ -10,11 +11,12 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
-@Order(1)                          // primeiro filtro a rodar
+@Order(1)
 @RequiredArgsConstructor
 public class RateLimitFilter implements Filter {
 
     private final CheckRateLimitUseCase checkRateLimit;
+    private final MeterRegistry meterRegistry;
 
     @Override
     public void doFilter(ServletRequest req,
@@ -24,22 +26,33 @@ public class RateLimitFilter implements Filter {
         HttpServletRequest  request  = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        String apiKey   = request.getHeader("X-API-Key");
-        String endpoint = request.getRequestURI();
+        String path = request.getRequestURI();
 
+        if (path.startsWith("/actuator")) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        String apiKey   = request.getHeader("X-API-Key");
+        String endpoint = path;
 
         if (apiKey == null || apiKey.isBlank()) {
+            meterRegistry.counter("ratelimit.requests",
+                    "status", "unauthorized",
+                    "endpoint", endpoint).increment();
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.getWriter().write("{\"error\": \"Missing X-API-Key header\"}");
             return;
         }
 
         RateLimitResult result = checkRateLimit.execute(apiKey, endpoint);
-
-
         addRateLimitHeaders(response, result);
 
         if (!result.isAllowed()) {
+            meterRegistry.counter("ratelimit.requests",
+                    "status", "blocked",
+                    "endpoint", endpoint,
+                    "client", apiKey).increment();
             response.setStatus(429);
             response.setContentType("application/json");
             response.getWriter().write(
@@ -48,6 +61,11 @@ public class RateLimitFilter implements Filter {
             );
             return;
         }
+
+        meterRegistry.counter("ratelimit.requests",
+                "status", "allowed",
+                "endpoint", endpoint,
+                "client", apiKey).increment();
 
         chain.doFilter(req, res);
     }
